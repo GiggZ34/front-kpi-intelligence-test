1b80a621be5561dcfd1d38fef2d7ef6cea59ba90
Rivalan Guillaume <guillaume.rivalan@gmail.com>
2025-02-10 00:23:03 +0100

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+sleep 5
 # Modify environment.js with the correct URLs
+chmod -x /usr/share/nginx/html/assets/environment.js.template
 envsubst <  /usr/share/nginx/html/assets/environment.js.template >  /usr/share/nginx/html/assets/environment.js
 
 # Start NGINX
